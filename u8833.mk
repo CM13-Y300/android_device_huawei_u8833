5b7731a96240a092b3f2dd89a1106498b9d510ba
Kyle Harrison <khwebmail@gmail.com>
2014-11-23 00:36:27 +0000

    Update properties for L

--- u8833.mk
+++ u8833.mk
@@ -44,9 +44,9 @@ PRODUCT_COPY_FILES += \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.confg.hw_appfsversion=U8833V4_4_SYSIMG \
-    ro.confg.hw_appsbootversion=U8833V4_4_APPSBOOT \
-    ro.confg.hw_appversion=U8833V4_4_KERNEL
+    ro.confg.hw_appfsversion=U8833V5_0_SYSIMG \
+    ro.confg.hw_appsbootversion=U8833V5_0_APPSBOOT \
+    ro.confg.hw_appversion=U8833V5_0_KERNEL
 
 PRODUCT_PROPERTY_OVERRIDES += \
     gsm.version.baseband=2030 \
