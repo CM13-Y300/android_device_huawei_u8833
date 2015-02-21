1fbeefaf7b914f7cf998f5664069160860b9e8b8
Kyle Harrison <khwebmail@gmail.com>
2015-02-21 22:32:45 +0000

    audio/msm7627a: Fix to apply SRS post proc effects on LPA stream
    - LPA playback progress flag is not updated properly as per new LPA design, due to this SRS post proc effects are not applied on LPA stream.
    - Fix to update LPA playback progress flag as per new design (cherry picked from commit 5d54f98b6c65880ec9a7d09cf033112da2fc375b)
    
    Change-Id: I0c6613a0c72f5892694fe23dbf738e3c808f3801

--- audio/AudioHardware.cpp
+++ audio/AudioHardware.cpp
@@ -3549,6 +3549,7 @@ status_t AudioHardware::AudioSessionOutLPA::openAudioSessionDevice( )
     } else {
         //initCheck = true;
         ALOGV("pcm_lp_dec: pcm_lp_dec Driver opened");
+        lpa_playback_in_progress = true;
     }
 
 	start();
@@ -3997,6 +3998,7 @@ void AudioHardware::AudioSessionOutLPA::reset()
     status_t status = NO_ERROR;
     bufferDeAlloc();
     ::close(afd);
+    lpa_playback_in_progress = false;
     ALOGD("AudioSessionOutLPA::reset() complete");
 }
 
