a7b961ba80f99fa89b25ee359f0a619502e3117f
knoelli <olt78@web.de>
2016-06-06 20:33:10 +0200

    BoardConfig.mk: Update Wifi modules configuration
    Updates to use remittor's changes from jsr-d9/android_hardware_atheros_wifi_ath6kl to improve wifi stability

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,19 +32,6 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/huawei/u8833/bluetooth
 # Kernel
 TARGET_KERNEL_CONFIG := u8833_defconfig
 
-# These currently have to go to the ramdisk for wlan_detect to pick them up.
-# Hopefully they can join their friends at $(KERNEL_MODULES_OUT) soon. :(
-KERNEL_EXTERNAL_MODULES:
-	mkdir -p $(TARGET_ROOT_OUT)/wifi
-	rm -rf $(TARGET_OUT_INTERMEDIATES)/ath6kl-huawei
-	cp -a hardware/atheros/wifi/ath6kl-huawei $(TARGET_OUT_INTERMEDIATES)/
-	$(MAKE) -C $(TARGET_OUT_INTERMEDIATES)/ath6kl-huawei/cfg80211 KERNEL_OUT=$(KERNEL_OUT) ARCH="arm" CROSS_COMPILE="arm-eabi-" modules
-	$(MAKE) -C $(TARGET_OUT_INTERMEDIATES)/ath6kl-huawei/ar6000 KERNEL_OUT=$(KERNEL_OUT) ARCH="arm" CROSS_COMPILE="arm-eabi-" modules
-	$(TARGET_OBJCOPY) --strip-unneeded $(TARGET_OUT_INTERMEDIATES)/ath6kl-huawei/cfg80211/cfg80211.ko $(TARGET_ROOT_OUT)/wifi/cfg80211.ko
-	$(TARGET_OBJCOPY) --strip-unneeded $(TARGET_OUT_INTERMEDIATES)/ath6kl-huawei/ar6000/ar6000.ko $(TARGET_ROOT_OUT)/wifi/ar6000.ko
-
-TARGET_KERNEL_MODULES := KERNEL_EXTERNAL_MODULES
-
 # Partitions
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824 # 0x40000000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1207943168 # 0x47FFC000
@@ -66,3 +53,31 @@ WIFI_DRIVER_FW_PATH_P2P := "p2p"
 
 WIFI_DRIVER_MODULE_NAME := "ar6000"
 WIFI_DRIVER_MODULE_PATH := "/data/misc/wifi/load/ar6000.ko"
+WIFI_DRIVER_IFACE_NAME := "eth"
+
+WIFI_PARAMS += WIFI_FIX_HUAWEI_BUTCHERING="true"
+WIFI_PARAMS += WIFI_POWER_TUNED="true"
+WIFI_PARAMS += AR6005_SELF_INIT_SUPPORT="true"
+
+WIFI_PARAMS += WIFI_DRIVER_IFACE_NAME="$(WIFI_DRIVER_IFACE_NAME)"
+WIFI_PARAMS += WIFI_DRIVER_MODULE_PATH="$(WIFI_DRIVER_MODULE_PATH)"
+WIFI_PARAMS += WIFI_DRIVER_MODULE_NAME="$(WIFI_DRIVER_MODULE_NAME)"
+
+WIFI_PARAMS += AR6003_MAC_FILE="softmac"
+WIFI_PARAMS += AR6003_REV2_BOARD_POWER_PA_FILE="../../data/misc/wifi/Cal_powerTuned_pa.bin"
+WIFI_PARAMS += AR6003_HW_FW_DIR="ath6k"
+WIFI_PARAMS += AR6003_HW_BOARD_DATA_FILE="load/caldata.bin"
+WIFI_PARAMS += AR6003_HW_DEFAULT_BOARD_DATA_FILE="ath6k/bdata.SD31.bin"
+
+# These currently have to go to the ramdisk for wlan_detect to pick them up.
+# Hopefully they can join their friends at $(KERNEL_MODULES_OUT) soon. :(
+KERNEL_EXTERNAL_MODULES:
+	mkdir -p $(TARGET_ROOT_OUT)/wifi
+	rm -rf $(TARGET_OUT_INTERMEDIATES)/ath6kl-huawei
+	cp -a hardware/atheros/wifi/ath6kl-huawei $(TARGET_OUT_INTERMEDIATES)/
+	$(MAKE) -C $(TARGET_OUT_INTERMEDIATES)/ath6kl-huawei/cfg80211 KERNEL_OUT=$(KERNEL_OUT) ARCH="arm" CROSS_COMPILE="arm-eabi-" modules
+	$(MAKE) -C $(TARGET_OUT_INTERMEDIATES)/ath6kl-huawei/ar6000 KERNEL_OUT=$(KERNEL_OUT) ARCH="arm" CROSS_COMPILE="arm-eabi-" $(WIFI_PARAMS) modules
+	$(TARGET_OBJCOPY) --strip-unneeded $(TARGET_OUT_INTERMEDIATES)/ath6kl-huawei/cfg80211/cfg80211.ko $(TARGET_ROOT_OUT)/wifi/cfg80211.ko
+	$(TARGET_OBJCOPY) --strip-unneeded $(TARGET_OUT_INTERMEDIATES)/ath6kl-huawei/ar6000/ar6000.ko $(TARGET_ROOT_OUT)/wifi/ar6000.ko
+
+TARGET_KERNEL_MODULES := KERNEL_EXTERNAL_MODULES
